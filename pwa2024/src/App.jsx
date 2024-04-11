4d2ca5752356a54bee4a3fd70c7cec31421a28e0
joaquin <joaquin.iri25@gmail.com>
2024-04-11 14:22:51 -0300

    pasando algunos archivos .js a .jsx sino no me andan algunas extensiones

---
+++ pwa2024/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className=" text-3xl font-bold  text-yellow-700 ">
+     Hello world!
+    <div>
+      <h2>hola putos</h2>
+    </div>
+    </div>
+
+  
+  );
+}
+
+export default App;
