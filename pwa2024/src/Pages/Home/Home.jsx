4a458c74bc8eb68bb2c07a4936b06a504556cc25
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-04-24 00:32:05 -0300

    Lista de Tareas básico

--- pwa2024/src/Pages/Home/Home.jsx
+++ pwa2024/src/Pages/Home/Home.jsx
@@ -2,13 +2,17 @@ import Titulo from "../../Components/Titulo/Titulo";
 import Tarea from "../../Components/Tarea/Tarea";
 
 export default function Home(){
+    const tareas = [
+        <Tarea descripcion="Andar en Bicicleta" estaCompletada={false}/>,
+        <Tarea descripcion="Correr en el parque" estaCompletada={true}/>
+    ]
+    const listaTareas = tareas.map(tarea => <li>{tarea}</li>);
     return (
         <>
             <Titulo texto="Tareas" />
-                <ul>
-                    <Tarea descripcion="Andar en Bicicleta" estaCompletada={false}/>
-                    <Tarea descripcion="Correr en el parque" estaCompletada={true}/>
-                </ul>
+            <ul>
+                {listaTareas}
+            </ul>
         </>
     )
 }
