cf4c66d2cd9acf6f376d8e212c61bab87461c696
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-04-24 00:57:24 -0300

    Lista de Tareas desde arreglo de tareas

--- pwa2024/src/Pages/Home/Home.jsx
+++ pwa2024/src/Pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import Titulo from "../../Components/Titulo/Titulo";
+import { tareas } from "../../datos/tareas";
 import Tarea from "../../Components/Tarea/Tarea";
 
 export default function Home(){
-    const tareas = [
-        <Tarea descripcion="Andar en Bicicleta" estaCompletada={false}/>,
-        <Tarea descripcion="Correr en el parque" estaCompletada={true}/>
-    ]
-    const listaTareas = tareas.map(tarea => <li>{tarea}</li>);
+    const listaTareas = tareas.map(tarea =>
+        <li key={tarea.id}>
+            <Tarea descripcion={tarea.descripcion} estaCompletada={tarea.estaCompletada}/>
+        </li>);
     return (
         <>
             <Titulo texto="Tareas" />
---
+++ pwa2024/src/datos/tareas.jsx
@@ -0,0 +1,12 @@
+export const tareas = [
+    {
+        id: 0,
+        descripcion: "Andar en Bicicleta",
+        estaCompletada : false
+    },
+    {
+        id: 1,
+        descripcion: "Correr en el parque",
+        estaCompletada: true
+    }
+]
