c861d2f9294f45947ba74224b0f84043f67a150b
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-05-02 13:12:36 -0300

    Comentarios utiles

--- pwa2024/src/Components/Tarea/Tarea.jsx
+++ pwa2024/src/Components/Tarea/Tarea.jsx
@@ -5,13 +5,13 @@ export default function Tarea({id, descripcion, estaCompletada}){
             setCompleta(!completa);
             const tareasLocalStrg = JSON.parse(localStorage.getItem('tareas'));
             const tareasActualizadas = tareasLocalStrg.map((tarea) =>{//aprovecho el map para cambiar el valor en localStorage
-                if (tarea.id === id){
+                if (tarea.id === id){ //Tengo que usar el id del objeto, no del arreglo, sino el argumento seria asi: (tarea, indiceArreglo)
                     return {...tarea, estaCompletada: !completa};
-                }else{
+                }else{      //Actualizo el objeto
                     return tarea;
-                }
+                }           //Sino sigue igual
             });
-            localStorage.setItem('tareas', JSON.stringify(tareasActualizadas));
+            localStorage.setItem('tareas', JSON.stringify(tareasActualizadas)); //El nuevo arreglo actualizado es seteado en localStorage
         }
     return (
         <>
