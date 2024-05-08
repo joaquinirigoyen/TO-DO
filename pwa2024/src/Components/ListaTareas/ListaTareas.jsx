034d1fc0bbb903c7bed7d5154cbaf3036db83eec
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-05-08 00:31:09 -0300

    Si ya no hay tareas en total

--- pwa2024/src/Components/ListaTareas/ListaTareas.jsx
+++ pwa2024/src/Components/ListaTareas/ListaTareas.jsx
@@ -2,7 +2,9 @@ import Tarea from '../Tarea/Tarea';
 
 export default function ListaTareas({tareasA, tareasF, cambiarEstado, eliminarTarea}){
     let tareas =tareasA;
+    if (tareas.length===0){return <h1>Se acabaron las tareas, puedes descansar por hoy</h1>}
     if (tareasF.length !== 0){tareas = tareasF};
+
 
     const listaTareas = tareas.map(tarea =>{
         return (
